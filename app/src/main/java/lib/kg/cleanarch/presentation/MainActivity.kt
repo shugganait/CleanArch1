ae9546978588e62d12a2c43253d0387e773e6963
shugganait <daniarm209@gmail.com>
2023-06-30 16:39:34 +0600

    шш

--- app/src/main/java/lib/kg/cleanarch/presentation/MainActivity.kt
+++ app/src/main/java/lib/kg/cleanarch/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package lib.kg.cleanarch
+package lib.kg.cleanarch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import lib.kg.cleanarch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
